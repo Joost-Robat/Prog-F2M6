73474188646edbc57120eedb75c7049fe9efe377
Joost-Robat <30769@ma-web.nl>
2021-11-22 14:49:59 +0100

    Initial commit.

---
+++ Database - prog/Database - prog/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Database___prog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
