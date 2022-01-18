e0833158de35bc368427d296a2bcdb76ecbccf10
Joost-Robat <30769@ma-web.nl>
2022-01-18 11:51:25 +0100

    Redid database.

---
+++ Database - prog/Database - prog/Klaslokaal.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Database___prog
+{
+    public class Klaslokaal
+    {
+        private int counter;
+        private bool read;
+        public Klaslokaal()
+        {
+
+        }
+
+        public void AddToClass()
+        {
+            Console.WriteLine("Person added:\n");
+            string added = Console.ReadLine();
+            Console.WriteLine("Added persons age:\n");
+            string addedAge = Console.ReadLine();
+            Console.WriteLine("Added persons role:\n");
+            string addedRole = Console.ReadLine();
+            Console.WriteLine("Save changes and exit?\nPlease make your choice by typing Yes or No (or x to discard all progress)\n:");
+            read = true;
+            while(read == true)
+            {
+                string userInput = Console.ReadLine();
+                if (userInput == "yes" || userInput == "Yes")
+                {
+                    string[] input = { added, addedAge, addedRole };
+                    string fileName = "person" + counter + ".txt";
+                    File.WriteAllLinesAsync(fileName, input);
+                    
+                }
+                else if (userInput.toLower() == "no")
+                {
+                    read = false;
+                    return;
+                }
+                else if (userInput == "x")
+                {
+                    
+                }
+                else
+                {
+                    Console.WriteLine("Input invalid, please try again.");
+                    return;
+                }
+            }
+        }
+        protected void ReadFromClass()
+        {
+            Console.WriteLine("Input ID to read from database:\n");
+            string userInput = Console.ReadLine();
+            
+        }
+    }
+}
--- Database - prog/Database - prog/Program.cs
+++ Database - prog/Database - prog/Program.cs
@@ -1,12 +1,25 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace Database___prog
 {
-    class Program : Inventory
+    class Program : Klaslokaal
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+            while (true)
+            {
+                Console.WriteLine("Input 1 to add a person to the class, input 2 to read a person by inputting their ID\n:");
+                string userInput = Console.ReadLine();
+                if(userInput == "1")
+                {
+                    
+                }
+            }
         }
     }
 }
